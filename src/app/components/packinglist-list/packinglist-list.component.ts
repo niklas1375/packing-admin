6663c1011522f7713f4f71599515f8918066a258
Niklas <niklas1375@gmail.com>
2024-10-01 13:10:46 +0200

    list filtered to type

--- src/app/components/packinglist-list/packinglist-list.component.ts
+++ src/app/components/packinglist-list/packinglist-list.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PackingHelperService } from '../../services/packing-helper.service';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { ValueHelpPackinglist } from '../../types/value-help-packinglist';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-packinglist-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, MatListModule, MatIconModule],
   templateUrl: './packinglist-list.component.html',
-  styleUrl: './packinglist-list.component.scss'
+  styleUrl: './packinglist-list.component.scss',
 })
 export class PackinglistListComponent {
+  packingLists$!: Observable<ValueHelpPackinglist[]>;
+  packingListType: string;
+  constructor(
+    private packingBackend: PackingHelperService,
+    private route: ActivatedRoute
+  ) {
+    this.packingListType = '';
+  }
 
+  ngOnInit(): void {
+    const urlSegments = this.route.snapshot.url;
+    this.packingListType = urlSegments[urlSegments.length - 1 || 0].path;
+    this.packingLists$ = this.packingBackend.getPackingListsOfType(
+      this.packingListType
+    );
+  }
 }
