464916b86cfda6cf9dbcd3478db3c363e8fa50b0
Niklas <niklas1375@gmail.com>
2024-10-11 16:15:13 +0200

    packing list detail component

--- src/app/components/packinglist-details/packinglist-details.component.ts
+++ src/app/components/packinglist-details/packinglist-details.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { PackingHelperService } from '../../services/packing-helper.service';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PackingList } from '../../types/packing-list';
 import { AppService } from '../../services/app.service';
@@ -23,6 +23,7 @@ import { PackingItem } from '../../types/packing-item';
 import { EllipsisPipe } from '../../pipes/ellipsis.pipe';
 import { PackingCategory } from '../../types/packing-category';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-packinglist-details',
@@ -37,6 +38,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
     MatListModule,
     MatIconModule,
     MatExpansionModule,
+    MatSnackBarModule,
     AsyncPipe,
     EllipsisPipe,
     RouterLink,
@@ -48,6 +50,7 @@ export class PackinglistDetailsComponent {
   packingList$!: Observable<PackingList>;
   form!: FormGroup;
   listid?: string;
+  listtype?: string;
   loading = false;
   submitting = false;
   submitted = false;
@@ -87,24 +90,38 @@ export class PackinglistDetailsComponent {
     'entertainment',
     'other',
   ];
+  categoryBackMapping = {
+    Kleidung: 'clothing',
+    'Hygiene & Co.': 'toiletries',
+    Ausrüstung: 'gear',
+    Organisatorisches: 'organisational',
+    Unterhaltung: 'entertainment',
+    sonstiges: 'other',
+  };
 
   constructor(
     private formBuilder: FormBuilder,
     private packingBackend: PackingHelperService,
     private route: ActivatedRoute,
-    private appService: AppService
+    private appService: AppService,
+    private snackBar: MatSnackBar,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.packinglistTypes$ = this.packingBackend.getPackingListTypes();
     this.listid = this.route.snapshot.params['listid'];
+    this.listtype = this.route.snapshot.params['type'];
 
     this.form = this.formBuilder.group({
       name: ['', Validators.required],
       type: ['', Validators.required],
     });
 
-    if (this.listid === 'new') return;
+    if (this.listtype) {
+      this.f['type'].patchValue(this.listtype);
+      return;
+    }
 
     this.loading = true;
     this.packingList$ = this.packingBackend.getPackingListWithItems(
@@ -127,11 +144,12 @@ export class PackinglistDetailsComponent {
     });
   }
 
-  onItemEdit($event: MouseEvent, _t60: PackingItem) {
-    throw new Error('Method not implemented.');
-  }
-  onItemDelete($event: MouseEvent, _t60: PackingItem) {
-    throw new Error('Method not implemented.');
+  onItemDelete(item: PackingItem) {
+    const canDelete = confirm(
+      `Packitem ${item.name} von Liste ${this.f['name'].getRawValue()} löschen?`
+    );
+    if (!canDelete) return;
+    this.packingBackend.deletePackingItemFromList(this.listid!, item.item_id);
   }
 
   get f() {
@@ -142,20 +160,38 @@ export class PackinglistDetailsComponent {
     this.changed = true;
   }
 
-  onSubmit() {
+  async onSubmit() {
     this.submitted = true;
 
     if (this.form.invalid) {
+      this.snackBar.open('Es fehlt Name und/oder Typ');
       return;
     }
 
     this.submitting = true;
-    this.savePackinglist();
+    const returnItem$: Observable<PackingList> | undefined =
+      this.savePackinglist();
+    if (!returnItem$) return;
+    if (!!this.listtype) {
+      returnItem$.subscribe((packingList) => {
+        this.router.navigate(['/packinglist/' + packingList.id]);
+      });
+    } else {
+      window.location.reload();
+    }
+  }
+
+  mapCategory(category: string) {
+    return (this.categoryBackMapping as any)[category];
   }
 
   private savePackinglist() {
-    return this.listid === 'new'
+    if (!this.form.value['name'] || !this.form.value['type']) {
+      this.snackBar.open('Es fehlt Name und/oder Typ');
+      return;
+    }
+    return !!this.listtype
       ? this.packingBackend.createPackingList(this.form.value)
-      : this.packingBackend.updatePackingList(this.listid, this.form.value);
+      : this.packingBackend.updatePackingList(this.listid!, this.form.value);
   }
 }
